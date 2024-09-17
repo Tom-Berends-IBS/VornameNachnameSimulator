e070576ca598fe63a4a02dffbf3abfc87afec065
Tom-Berends-EWE <tom.berends@ewe-netz.de>
2024-09-17 13:37:42 +0200

    Bau des Hauptinhalts

--- src/main/java/de/nachname/Main.java
+++ src/main/java/de/nachname/Main.java
@@ -3,6 +3,9 @@ package de.nachname;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -15,8 +18,14 @@ public class Main extends Application {
     public void start(final Stage primaryStage) {
         final BorderPane root = new BorderPane();
 
+        final SplitPane splitPane = new SplitPane();
+        final TextArea textArea = new TextArea();
+        final ScrollPane scrollPane = new ScrollPane();
+        splitPane.getItems().addAll(textArea, scrollPane);
+
         final Label label = new Label("Herzlich Willkommen!");
 
+        root.setCenter(splitPane);
         root.setBottom(label);
         
         final Scene scene = new Scene(root);
