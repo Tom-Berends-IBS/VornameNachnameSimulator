b073315279f015b8ce6b55d201be01c6bd43ef54
Tom-Berends-EWE <tom.berends@ewe-netz.de>
2024-09-17 13:37:10 +0200

    Bau der Status-Zeile

--- src/main/java/de/nachname/Main.java
+++ src/main/java/de/nachname/Main.java
@@ -2,6 +2,7 @@ package de.nachname;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -13,6 +14,11 @@ public class Main extends Application {
     @Override
     public void start(final Stage primaryStage) {
         final BorderPane root = new BorderPane();
+
+        final Label label = new Label("Herzlich Willkommen!");
+
+        root.setBottom(label);
+        
         final Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
