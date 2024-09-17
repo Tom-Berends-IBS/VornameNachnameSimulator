3f834f20e8afce3020d445d16160af9db155a937
Tom-Berends-EWE <tom.berends@ewe-netz.de>
2024-09-17 13:35:00 +0200

    Einfügen einer Scene

--- src/main/java/de/nachname/Main.java
+++ src/main/java/de/nachname/Main.java
@@ -1,6 +1,8 @@
 package de.nachname;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,6 +12,10 @@ public class Main extends Application {
 
     @Override
     public void start(final Stage primaryStage) {
+        final BorderPane root = new BorderPane();
+        final Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
